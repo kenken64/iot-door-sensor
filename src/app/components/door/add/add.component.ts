38fe6a88a425a01ca92dd0e19c98db4c5b8ca8e9
Kenneth Phang <bunnyppl@gmail.com>
2019-01-11 17:35:55 +0800

    added export excel

--- src/app/components/door/add/add.component.ts
+++ src/app/components/door/add/add.component.ts
@@ -1,56 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { DoorService } from '../../../services/door.service';
-import  { Door } from '../../../model/door';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { DoorService } from "../../../services/door.service";
+import { Door } from "../../../model/door";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  selector: "app-add",
+  templateUrl: "./add.component.html",
+  styleUrls: ["./add.component.css"]
 })
 export class AddDoorComponent implements OnInit {
-
-  constructor(private svc: DoorService, private snackBar: MatSnackBar) { }
+  constructor(private svc: DoorService, private snackBar: MatSnackBar) {}
 
   doorForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    sensorAuth: new FormControl('', Validators.required),
+    name: new FormControl("", Validators.required),
+    sensorAuth: new FormControl("", Validators.required)
   });
 
   ngOnInit() {}
 
-  saveDoor(){
+  saveDoor() {
     let name = this.doorForm.get("name").value;
     let sensorAuth = this.doorForm.get("sensorAuth").value;
-    let d : Door = {
-        name:  name,
-        sensor_auth: sensorAuth,
-        status: "Closed",
-    }
+    let d: Door = {
+      name: name,
+      sensor_auth: sensorAuth,
+      status: "Closed"
+    };
 
-    let unsub = this.svc.getDoorBySensorAuth(sensorAuth).snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+    let unsub = this.svc
+      .getDoorBySensorAuth(sensorAuth)
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
       )
-    ).subscribe(doors => {
-      console.log(">>> " + doors);
-      console.log(">>>>>> " +doors.length);
-      if(doors.length == 0){
-        this.svc.saveDoor(d).subscribe((result)=>{
-          console.log("snack time !" + result);
-          let snackBarRef = this.snackBar.open('Door Added', 'Done', {
+      .subscribe(doors => {
+        if (doors.length == 0) {
+          this.svc.saveDoor(d).subscribe(result => {
+            console.log("snack time !" + result);
+            let snackBarRef = this.snackBar.open("Door Added", "Done", {
+              duration: 3000
+            });
+            unsub.unsubscribe();
+          });
+        } else {
+          let snackBarRef = this.snackBar.open("Door already exist.", "Done", {
             duration: 3000
           });
-          unsub.unsubscribe();
-        });
-      }else{
-        let snackBarRef = this.snackBar.open('Door already exist.', 'Done', {
-          duration: 3000
-        });
-      }
-    });
-    
+        }
+      });
   }
 }
--- src/app/components/door/door.component.ts
+++ src/app/components/door/door.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { DoorService } from '../../services/door.service';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { DoorService } from "../../services/door.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from "@angular/material";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-door',
-  templateUrl: './door.component.html',
-  styleUrls: ['./door.component.css']
+  selector: "app-door",
+  templateUrl: "./door.component.html",
+  styleUrls: ["./door.component.css"]
 })
 export class DoorComponent implements OnInit {
   doors: any;
 
-  constructor(private doorSvc: DoorService,
-    iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) 
-  { 
+  constructor(
+    private doorSvc: DoorService,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
     iconRegistry.addSvgIcon(
-      'door_open',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/door_open.svg'));
+      "door_open",
+      sanitizer.bypassSecurityTrustResourceUrl("assets/door_open.svg")
+    );
 
-      iconRegistry.addSvgIcon(
-        'door_closed',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/door_closed.svg'));
+    iconRegistry.addSvgIcon(
+      "door_closed",
+      sanitizer.bypassSecurityTrustResourceUrl("assets/door_closed.svg")
+    );
   }
 
   ngOnInit() {
@@ -30,22 +34,26 @@ export class DoorComponent implements OnInit {
 
   getDoorList() {
     // Use snapshotChanges().map() to store the key
-    this.doorSvc.getAllDoor().snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+    this.doorSvc
+      .getAllDoor()
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
       )
-    ).subscribe(doors => {
-      this.doors = doors;
-      this.doors.sort((n1 , n2)=>{
-        if (n1.name > n2.name) {
+      .subscribe(doors => {
+        this.doors = doors;
+        console.log(this.doors);
+        this.doors.sort((n1, n2) => {
+          if (n1.name > n2.name) {
             return 1;
-        }
-        if (n1.name < n2.name) {
+          }
+          if (n1.name < n2.name) {
             return -1;
-        }
-        return 0;
+          }
+          return 0;
+        });
       });
-    });
   }
-
 }
