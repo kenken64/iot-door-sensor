b87bddf0f4ea11daaf87f09855f7a6347e77d534
unknown <bunnyppl@hotmail.com>
2019-04-27 04:02:56 +0800

    clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firmware_api_url: 'https://ota-firmware.kennethphang.asia/api/',
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyB1GiFDJas7GgftbAuFgkNAajm4e0DLoMM",
+    authDomain: "door-sensor-proj.firebaseapp.com",
+    databaseURL: "https://door-sensor-proj.firebaseio.com",
+    projectId: "door-sensor-proj",
+    storageBucket: "door-sensor-proj.appspot.com",
+    messagingSenderId: "60505252179"
   }
 };
